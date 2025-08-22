23901fa0b44a62a027a9292db38575f079429500
Illia Rozhok <i.rozhok@thein1.com>
2025-08-23 01:43:51 +0300

    add themes

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,53 +5,50 @@ import { apiGet } from "@/lib/api";
 import { User } from "@/types";
 import { ROUTES } from "@/router";
 
+import { ModeToggle } from "@/components/MoodToggle/MoodToggle";
+import { useRouter } from "next/navigation";
+
 export default function AppPage() {
   const [me, setMe] = useState<User | null>(null);
   const [error, setError] = useState<string | null>(null);
 
+  const router = useRouter();
   useEffect(() => {
     apiGet<User>(ROUTES.AUTH.ME)
       .then(setMe)
-      .catch(() => setError("Not authenticated"));
+      .catch(() => router.push("/"));
   }, []);
-
-  if (error) {
-    return (
-      <div className="p-6">
-        <p className="mb-4">Вы не авторизованы.</p>
-        <a
-          href="/"
-          className="inline-block px-3 py-2 rounded-lg bg-black text-white"
-        >
-          Войти
-        </a>
-      </div>
-    );
-  }
 
   if (!me) return <div className="p-6">Загрузка…</div>;
 
   return (
-    <div className="p-6 flex items-center gap-4">
-      {me.avatarUrl ? (
-        // если хочешь оптимизировать:
-        // <Image src={me.avatarUrl} alt="avatar" width={40} height={40} className="rounded-full" />
-        <img
-          src={me.avatarUrl}
-          alt="avatar"
-          className="w-10 h-10 rounded-full"
-        />
-      ) : null}
-      <div>
+    <div className="p-6 flex items-center justify-between gap-4">
+      <div className="flex items-center">
+        {me.avatarUrl ? (
+          // если хочешь оптимизировать:
+          // <Image src={me.avatarUrl} alt="avatar" width={40} height={40} className="rounded-full" />
+          <img
+            src={me.avatarUrl}
+            alt="avatar"
+            className="w-10 h-10 rounded-full mr-3"
+          />
+        ) : null}
+
         <div className="font-medium">{me.userName ?? "User"}</div>
-        <div className="text-sm text-gray-500">{me.email}</div>
+        {/* <div className="text-sm text-gray-500">{me.email}</div> */}
       </div>
-      <a
-        href={`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`}
-        className="ml-auto px-3 py-2 rounded-lg border"
-      >
-        Выйти
-      </a>
+
+      <div className="flex items-center">
+        <div className="mr-3">
+          <ModeToggle />
+        </div>
+        <a
+          href={`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`}
+          className="ml-auto px-3 py-2 rounded-lg border"
+        >
+          Exit
+        </a>
+      </div>
     </div>
   );
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="min-h-screen bg-background">{children}</div>
+    <html className="theme-purple" suppressHydrationWarning lang="en">
+      <body className={"bg-background text-foreground"}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
